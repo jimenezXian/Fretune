4fc6a362e27a96c8857fc595aba9c549b46100b8
Christian Jimenez <christianjimenezfael@gmail.com>
2026-04-09 01:17:53 -0400

    read old documentation, updated to nondepreciated expo plugin. Lite music theory reading

--- components/tuning/TuneSet.tsx
+++ components/tuning/TuneSet.tsx
@@ -1,27 +1,45 @@
+import { useSelectedInstrument, useTunerStore } from "@/store/useTunerStore";
+import { useRouter } from "expo-router";
 import React from "react";
-import { TouchableOpacity } from "react-native";
-import { Typography } from "../ui";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Icon, Typography } from "../ui";
 
-// export type TTuning {
-//     name: string;
-// }
 
 interface ITuneSetProps {
+    tuningId: string,
     name: string;
+
 }
 
-export function TuneSet({ name }: ITuneSetProps) {
+export function TuneSet({ name, tuningId }: ITuneSetProps) {
     /* ******************** Hooks ******************** */
+    const router = useRouter();
+    const instrument = useSelectedInstrument();
     
+    const setTuning = useTunerStore((state) => state.setTuning);
+
     /* ******************** Variables ******************** */
     /* ******************** Functions ******************** */
+    const selectTuning = () => {
+        setTuning(instrument.id, tuningId)
+        router.back();
+    }
+
     /* ******************** Effects ******************** */
     /* ******************** JSX ******************** */
     return (
-        <TouchableOpacity onPress={() => dispatch()}>
+        <TouchableOpacity onPress={selectTuning} style={s.container}>
             <Typography variant="p1">
                 {name}
             </Typography>
-        </TouchableOpacity>
+            <Icon name="chevron-right" />
+        </TouchableOpacity >
     );
 }
+
+const s = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    }
+})
